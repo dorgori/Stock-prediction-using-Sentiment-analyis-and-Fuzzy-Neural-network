9e7f3a86fa4513d514d0a8e70f9d753586c4a266
oren <orenys7@gmail.com>
2019-04-25 14:55:21 +0300

    Sentiment Analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,7 +5,7 @@ indicoio.config.api_key = '0b80d9da8f4e847bd018ef74e597ad62'
 
 mylist = ["b'@mitchellvii How about reparations for all the people who, did pay their student loans."]
 mylist.append("People who paid matter too! (Sarcasm obviously)'")
-mylist.append("http://www.People.com who paid matter too! (Sarcasm obviously)")
+mylist.append("    ")
 mylist.append("I love writing code!")
 mylist.append("http://www.yeay.com I love writing code!")
 #print(indicoio.emotion(["b'@mitchellvii How about reparations for all the people who did pay their student loans.",  "People who paid matter too! (Sarcasm obviously)'"]))
