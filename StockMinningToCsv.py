5f5a97e3b426151ec73c294be7a9e66de798c777
oren <orenys7@gmail.com>
2019-05-02 14:57:01 +0300

    needs to continue stockValue

--- StockMinningToCsv.py
+++ StockMinningToCsv.py
@@ -1,9 +1,41 @@
 import csv
 import yahoofinance as yf
+import datetime
+from datetime import timedelta
 
 #profile = yf.AssetProfile('AAPL')
 #profile.to_csv('AAPL-profile.csv')
 
 ### share params : share symbol, start time, end time
-share = yf.HistoricalPrices('AAPL','2019-01-01', '2019-02-02')
-share.to_csv('AAPL-prices.csv')
+'''
+choose symbol, since date, until date
+'''
+symbol = 'AAPL'
+since_date = '2019-01-05'
+until_date = '2019-02-05'
+
+now = datetime.datetime.now()
+day = now.strftime("%A")
+print(day)
+
+if day == 'Thursday':                       #needs to be friday
+    yesterday = now - timedelta(days=1)
+    print(yesterday.strftime('20%y-%m-%d'))
+    '''
+    complete missing data from the 2 previous days
+    '''
+    pass
+if day == 'Saturday':
+    pass
+
+share = yf.HistoricalPrices(symbol, since_date, until_date)
+#share.to_csv('Stock Values/'+since_date+'.csv')
+stockValue = share.to_dfs(list)
+
+
+
+with open('Stock Values/'+since_date+'.csv') as f:
+    reader = list(csv.reader(f))
+    print(reader)
+    f.close()
+
