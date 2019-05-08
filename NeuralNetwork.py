99f2ff5350b6ac9f66d696bd5752e82978a99735
Dor Itah <dor@amarel.net>
2019-05-08 13:08:58 +0300

    add tweets

---
+++ NeuralNetwork.py
@@ -0,0 +1,32 @@
+
+
+import glob
+import pandas as pd
+import numpy as np
+import math
+
+class NeuralNet():
+    def __init__(self,stateName):
+        self.createMoodList(stateName)
+
+
+    def createMoodList(self,stateName):
+        self.path = 'Public Mood/' + stateName + '/'
+        self.moodFile = glob.glob(self.path + "*.csv")[0]
+        df = pd.read_csv(self.moodFile)
+        self.joy_values = df['joy']
+        self.surprise_value = df['surprise']
+        self.mood_list = self.joy_values+self.surprise_value
+        print(self.mood_list)
+
+    def calcGausianFunction(self, values_list):
+        self.mean = np.mean(values_list)
+        for xi in values_list:
+            Mik_xi = math.exp(-((xi-self.mean)**2)/self.calcVariance(xi,self.mean))
+
+    def calcVariance(self,xi, mean):
+        return 0.5*((xi-mean)**2)
+
+
+if __name__ == "__main__":
+    window = NeuralNet('California')
